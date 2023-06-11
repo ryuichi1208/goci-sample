f45cb057aa6956e7e107804cc93bd8a1037b3a0d
ryuichi1208 <ryucrosskey@gmail.com>
2023-06-11 18:39:16 +0900

    fix

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
-// 足し算して返す
+import (
+	"fmt"
+	"time"
+)
+
 func calc(x int, y int) int {
 	return x + y
 }
 
 func main() {
 	calc(1, 2)
+
+	t := calc3(time.Now())
+	fmt.Println(t)
+
+	//時刻型で21:00を表す
+	t1 := time.Date(2023, 12, 1, 21, 0, 0, 0, time.Local)
+	fmt.Println(calc3(t1))
+
+}
+
+func calc2() (time.Time, time.Time) {
+	now := time.Now()
+	return now, now.Add(time.Hour)
+}
+
+func calc3(t time.Time) time.Time {
+	return t.Add(time.Hour * 3)
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 // calc関数のテスト
 func TestCalc(t *testing.T) {
@@ -19,6 +22,35 @@ func TestCalc(t *testing.T) {
 		result := calc(testCase.x, testCase.y)
 		if result != testCase.expected {
 			t.Errorf("calc(%d, %d) = %d, expected %d", testCase.x, testCase.y, result, testCase.expected)
+		}
+	}
+}
+
+func TestCalc3(t *testing.T) {
+	// テストケースの入力と期待される出力を定義します
+	testCases := []struct {
+		input    time.Time
+		expected time.Time
+	}{
+		{
+			input:    time.Date(2023, time.June, 11, 12, 0, 0, 0, time.UTC),
+			expected: time.Date(2023, time.June, 11, 15, 0, 0, 0, time.UTC),
+		},
+		{
+			input:    time.Date(2023, time.June, 11, 23, 45, 0, 0, time.UTC),
+			expected: time.Date(2023, time.June, 12, 2, 45, 0, 0, time.UTC),
+		},
+		// 追加のテストケースを必要に応じて追加できます
+	}
+
+	// テストケースを順番に実行します
+	for _, testCase := range testCases {
+		// calc3 関数を実行して結果を取得します
+		result := calc3(testCase.input)
+
+		// 結果が期待される値と一致するかテストします
+		if result != testCase.expected {
+			t.Errorf("calc3(%v) の結果は %v であり、期待される結果 %v と一致しません", testCase.input, result, testCase.expected)
 		}
 	}
 }
